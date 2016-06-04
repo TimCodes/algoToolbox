12481a5671753bfb09a38d338c61133bce52a1e5
Tim Hardy <thardy@sbt.local>
2016-06-03 18:21:47 -0600

    added week three sutff

--- week3/greedy_algorithms_starter_files/change/change.py
+++ week3/greedy_algorithms_starter_files/change/change.py
@@ -3,8 +3,16 @@ import sys
 
 def get_change(n):
     #write your code here
-    return n
+    coins     = [10,5, 1]
+    coinCount = 0
+    total     = 0
+    for coin in coins:
+        while total + coin <= n:
+            total += coin
+            coinCount +=1
+
+    return coinCount
 
 if __name__ == '__main__':
-    n = int(sys.stdin.read())
+    n = int(input())
     print(get_change(n))
