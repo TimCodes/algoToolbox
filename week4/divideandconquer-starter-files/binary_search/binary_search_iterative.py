fc90886b9b439b80d6097e0bd815792ba89e7a42
Tim Hardy <thardy@sbt.local>
2016-07-20 18:22:35 -0600

    worked on majority element

--- week4/divideandconquer-starter-files/binary_search/binary_search_iterative.py
+++ week4/divideandconquer-starter-files/binary_search/binary_search_iterative.py
@@ -2,8 +2,26 @@
 import sys
 
 def binary_search(a, x):
-    left, right = 0, len(a)
+
     # write your code here
+    left, right = 0, len(a) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        a_mid = a[mid]
+        if x == a_mid:
+            return mid
+
+        # left--mid--x--right
+        if a_mid < x:
+            left = mid + 1
+
+        # left--x--mid--right
+        elif x < a_mid:
+            right = mid - 1
+
+    return -1
+
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +37,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
--- week4/divideandconquer-starter-files/binary_search/binary_search_recursive.py
+++ week4/divideandconquer-starter-files/binary_search/binary_search_recursive.py
@@ -1,8 +1,22 @@
 # Uses python3
 import sys
 
+def binary_search_recursive(a, low, high, key):
+
+        if high < low:
+            return low -1
+
+        mid = low  + ((high - low) // 2)
+
+        if a[mid] == key:
+            return mid
+        elif key < a[mid]:
+            return binary_search_recursive(a, low, mid -1, key)
+        else:
+            return  binary_search_recursive(a, mid + 1, high, key)
 def binary_search(a, x):
     left, right = 0, len(a)
+    return binary_search_recursive(a, left, right, x)
     # write your code here
 
 def linear_search(a, x):
